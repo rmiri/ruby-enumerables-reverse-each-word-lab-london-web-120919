0896e8b49cc6a9c20e166bc2923296c26a1c3d5b
Renata Freitas <r.miriuk@gmail.com>
2019-11-17 13:29:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,12 @@ def reverse_each_word(array)
   #array.each {|string| string.split(" ")}
   #undefined method `each' for "Hello there, and how are you?":String
   newArr = array.split(" ")
-newArr.each {|word| word.reverse!}.join(" ")
+  newArr.each {|word| word.reverse!}.join(" ")
+end
+
+def reverse_each_word(array)
+  #array.each {|string| string.split(" ")}
+  #undefined method `each' for "Hello there, and how are you?":String
+  newArr = array.split(" ")
+  newArr.collect {|word| word.reverse!}.join(" ")
 end
