32104be457fee3d796a2b237423b3bf544dd8096
Renata Freitas <r.miriuk@gmail.com>
2019-11-17 13:21:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(array)
-  array.each {|string| string.reverse}
+  array.each {|string| string.split(" ")}
   #undefined method `each' for "Hello there, and how are you?":String
 end
